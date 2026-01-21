3d7427955c4cc7c5b6769269a865f4a4909423be
Suraj Sharma <surajsharma60923@gmail.com>
2026-01-21 11:48:48 +0530

    db cluster

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobverse-x783.onrender.com',
+    origin: process.env.NODE_ENV === 'production' 
+        ? 'https://jobverse-x783.onrender.com'
+        : ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
     credentials:true
 }
 
